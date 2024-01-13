85d2aaa6c1402e11ab95e430d619806f493f688f
Celso Fernandes <56808157+Celso-Fernandes-ON@users.noreply.github.com>
2024-01-13 20:34:03 -0300

    first commint

---
+++ src/FocusTimer/events.js
@@ -0,0 +1,33 @@
+import {controls} from './elements.js'
+import * as actions from './actions.js'
+import * as el from './elements.js'
+import { updateDisplays } from './timer.js'
+
+import state from './state.js'
+ 
+export function registerControls(){
+    controls.addEventListener('click',(event) => {
+        const action = event.target.dataset.action
+        if(typeof actions [action] != "function"){
+            return
+        }
+
+        actions [action]()
+
+        console.log(event.target.dataset.action)
+    })
+}
+export function setMinutes(){
+    el.minutes.addEventListener('focus',() =>{
+        el.minutes.textContent = ""
+    })
+    el.minutes.onkeypress = (Event) => /\d/.test(event.key)
+    el.minutes.addEventListener('blur',(event) =>{
+        let time = event.currentTarget.textContent
+        time = time > 60 ? 60 : time
+        state.minutes = time
+        state.seconds = 0
+        updateDisplays()
+        el.minutes.removeAttribute('contenteditable')
+    })
+}
